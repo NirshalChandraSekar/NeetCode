0f3adba57e23fb15bdf9d34abb6619cff859ee9d
NirshalChandraSekar <nirshalniru@gmail.com>
2025-04-23 23:33:26 -0500

    good node in binary treE

--- Trees/good_nodes.py
+++ Trees/good_nodes.py
@@ -6,12 +6,9 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def goodNodes(self, root) -> int:
+    def goodNodes(self, root):
         
         good_nodes = 0
-
-        print("root", root)
-        print("root.val", root.val)
 
         if not root:
             return good_nodes
@@ -23,12 +20,10 @@ class Solution:
         
         while stack:
             node, node_list = stack.pop()
-            print("node", node.val)
-            print("node_list", node_list)
             
             current_list = node_list + [node.val]
 
-            if node.val >= max(node_list):
+            if node.val >= max(current_list):
                 good_nodes += 1
 
             if node.right:
@@ -38,16 +33,20 @@ class Solution:
                 stack.append([node.left, current_list])
 
         return good_nodes
+            
 
 # Example usage:
 # Constructing a sample binary tree root=[2,1,1,3,null,1,5]
 root = TreeNode(2)
-root.left = TreeNode(1)
+root.left = TreeNode(1)     
 root.right = TreeNode(1)
 root.left.left = TreeNode(3)
-root.left.right = TreeNode(None)
+root.left.right = None
 root.right.left = TreeNode(1)
 root.right.right = TreeNode(5)
 solution = Solution()
 good_nodes = solution.goodNodes(root)
-print(good_nodes)  # Output: 4          
+print(good_nodes)  # Output: 4
+# Constructing a sample binary tree root=[3,1,4,3,null,1,5]
+
+            
